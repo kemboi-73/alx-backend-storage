050b0f2aa0a2d536b064899b3c82f6e554410861
kemboi-73 <kemboimichael52@gmail.com>
2024-01-23 14:27:09 +0000

    function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Contains a function that returns all students sorted by avg score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {
+                        "$avg": "$topics.score"
+                    }
+                }
+        },
+        {
+            "$sort":
+            {
+                "averageScore": -1
+            }
+        }
+    ])
